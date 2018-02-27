e8a7f35bfb909617090f4ea99c2e7fbd88c34816
glefevre2017 <glefevre2017@1-UC39-07.ad.campus-eni.fr>
2018-02-27 10:09:51 +0100

    correctif Ecran login | +Add AppliTestIHM

---
+++ src/fr/eni/ProjetVeterinaire/ihm/login/AppliTestIHM.java
@@ -0,0 +1,12 @@
+/*
+ * Auteur : Gauthier LEFEVRE 
+ * ENI
+ * Projet client - serveur JAVA  / Groupe 3
+ * 
+ */
+
+package src.fr.eni.ProjetVeterinaire.ihm.login;
+
+public class AppliTestIHM {
+
+}
